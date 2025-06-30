2429b99a77b0bd662b238c26d3989e2e6cb1ab56
Avinash <avin.roy001@gmail.com>
2025-06-30 22:35:47 +0530

    checking

--- xcountries/src/App.jsx
+++ xcountries/src/App.jsx
@@ -1,45 +1,41 @@
-import { useState, useEffect } from 'react';
-import Card from './Card';
-import './App.css';
-import { fetchData } from './api';
+import { useState, useEffect } from "react";
+import Card from "./Card";
+import "./App.css";
+import { fetchData } from "./api";
 
 function App() {
   const [info, setInfo] = useState([]);
   const [error, setError] = useState(null);
 
-    const getData = async () => {
-  try {
-    const ans = await fetchData();
-    setInfo(ans);
-  } catch (e) {
-    console.error("Error fetching data:"); 
-    console.error(e); 
-    setInfo([]);
-    setError("Failed to load data.");
-  }
-};
-
-
+  const getData = async () => {
+    try {
+      const ans = await fetchData();
+      setInfo(ans);
+    } catch (e) {
+      console.error("Error fetching data:");
+      setInfo([]);
+      setError("Failed to load data.");
+    }
+  };
 
   useEffect(() => {
     getData();
   }, []);
 
   return (
-  <>
-    {error && <p className="error">{error}</p>}
-    {Array.isArray(info) && info.length ? (
-      <div className="card">
-        {info.map((el) => (
-          <Card data={el} key={el.abbr} />
-        ))}
-      </div>
-    ) : (
-      !error && <p>Loading...</p>
-    )}
-  </>
-);
-
+    <>
+      {error && <p className="error">{error}</p>}
+      {Array.isArray(info) && info.length ? (
+        <div className="card">
+          {info.map((el) => (
+            <Card data={el} key={el.abbr} />
+          ))}
+        </div>
+      ) : (
+        !error && <p>Loading...</p>
+      )}
+    </>
+  );
 }
 
 export default App;
